af428714d5379aca50f1865c8bb638ab3b55e80f
David Li <li.davidm96@gmail.com>
2015-05-12 16:38:22 -0400

    Get specular working better (hack)

--- src/raytrace/lighting.rs
+++ src/raytrace/lighting.rs
@@ -15,15 +15,17 @@ fn get_ambient_color(material: &Material) -> Color {
 }
 
 fn get_diffuse_color(light: &Light, material: &Material,
-                     n: Vec3, l: Vec3) -> Color {
+                     n: Vec3, l: Vec3, distance: f64) -> Color {
+    // TODO: why do I need this abs
     ((*material).diffuse * (*light).color *
-        n.dot(l).max(0.0) * (*light).intensity)
+        n.dot(l).abs() * (*light).intensity / distance)
 }
 
 fn get_specular_color(material: &Material,
                       l: Vec3, v: Vec3, normal: Vec3) -> Color {
     let h = (v + l).normalized();
-    (*material).specular * (normal.dot(h).max(0.0).powf((*material).specular_exponent))
+    ((*material).specular *
+     (normal.dot(h).max(0.0).powf((*material).specular_exponent)))
 }
 
 pub fn get_color(scene: &Scene, materialobject: &MaterialObject,
@@ -39,10 +41,12 @@ pub fn get_color(scene: &Scene, materialobject: &MaterialObject,
             origin: collision - (direction * 0.00001),
             direction: direction
         };
+        let vec_to_light = light.position - collision;
+        let distance = vec_to_light.mag_squared().sqrt();
         let l = (light.position - collision).normalized();
         let v = new_ray.origin - new_ray.direction;
 
-        let diffuse = get_diffuse_color(&light, material, normal, l);
+        let diffuse = get_diffuse_color(&light, material, normal, l, distance);
         let specular = get_specular_color(material, l, v, normal);
         (prev_color + diffuse + specular, prev_rays + 1)
     })
